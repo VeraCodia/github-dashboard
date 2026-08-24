98842c707ae0a116101e84bb8aa2c9995b72dc26
Ayham Alhomsy <Ayham.Alhomsi@veracodia.com>
2026-08-24 13:51:25 +0300

    adding first test

---
+++ core/test/soso.test.ts
@@ -0,0 +1,10 @@
+/*
+    * fetch all pr with open status
+    * 1- get metrics: opened PRs count
+ */
+
+describe('soso test', () => {
+    it('should 1 + 2 equal 3 ', () => {
+        expect(1+2).toBe(3);
+    });
+});
